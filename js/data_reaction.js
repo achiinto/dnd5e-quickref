2397728cbc7f174821bd9e200aecad95bbf199b4
achiinto <achiinto@gmail.com>
2020-08-21 01:07:52 +0800

    Basic on reaction

--- js/data_reaction.js
+++ js/data_reaction.js
@@ -1,36 +1,36 @@
 data_reaction = [
     {
-        title: "Opportunity attack",
+        title: "機會性攻擊 Opportunity attack",
         icon: "crossed-swords",
-        subtitle: "Enemy leaves your reach",
+        subtitle: "敵人離開了你的近身攻擊範圍",
         description: "You can rarely move heedlessly past your foes without putting yourself in danger",
         reference: "PHB, pg. 195.",
         bullets: [
-            "Trigger: enemy creature leaves your reach.",
+            "啟機: 敵人離開了你的近身攻擊範圍時。",
             "Make one melee attack against the provoking creature.",
             "The attack interrupts the provoking creature's movement, occurring right before the creature leaves your reach.",
             "Creatures don't provoke an opportunity attack when they teleport or when someone or something moves them without using their movement, action, or reaction."
         ]
     },
     {
-        title: "Readied action",
+        title: "事先預備的行動 Readied action",
         icon: "stopwatch",
-        subtitle: "Part of your Ready action",
-        description: "Execute the reaction specified by your Ready action",
+        subtitle: "你的「預備」行動的一部分",
+        description: "反射性的行使你所預備的指定行動",
         reference: "PHB, pg. 193.",
         bullets: [
-            "Trigger: specified by your <i>Ready</i> action."
+            "啟機: 在你的「預備」行動中所指定。"
         ]
     },
     {
-        title: "Cast a spell",
+        title: "使用魔法 Cast a spell",
         icon: "magic-swirl",
-        subtitle: "Cast time of 1 reaction",
-        description: "Cast a spell with a casting time of 1 reaction",
+        subtitle: "一個反射動作內可啟動的魔法",
+        description: "使用一個可在一個反射動作內啟動的魔法",
         reference: "PHB, pg. 192.",
         bullets: [
-            "Trigger: specified by the spell.",
-            "For further details, see the <i>Cast a spell</i> action."
+            "啟機: 在魔法內容中所指定。",
+            "請參考「使用魔法」行動的細節。"
         ]
     }
 ]

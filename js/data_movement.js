e2004888fe5b7a5c2308bd995dad040ce11c5708
achiinto <achiinto@gmail.com>
2020-08-21 00:40:52 +0800

    Movement description completed

--- js/data_movement.js
+++ js/data_movement.js
@@ -9,7 +9,7 @@ data_movement = [
             "如果你已經移動了多過一次, 例如你的步行速度和飛行速度, 你可以在你的移動期間在不同的移動型式間變換。當你需要轉換移動方式時，在剩餘的可移動力中減去你已經動用了的移動消費便可。",
             "你可以移動經過對你無惡意的生物所處之空間。",
             "你只可以移動經過對你有惡意的生物所處之空間，如果對方至少比你大又或者細你兩個身位。",
-            "另一個生物的所處空間對你來說是一個「困難地型」（difficult terrain)。",
+            "另一個生物的所處空間對你來說是一個「困難地形」（difficult terrain)。",
             "無論那生物是友好又或是敵對，你都不能夠在其身處空間完結你的移動回合。"
         ]
     },
@@ -88,8 +88,8 @@ data_movement = [
         bullets: [
             "當你至少用腳行走了10呎遠後，你可以向前跳遠至你的體力值 (Strength score)的呎數距離。",
             "如果你在沒有助跑的情況下原地跳遠，你只能夠跳遠至一半的距離。",
-            "May involve a DC 10 Strength (Athletics) check to clear a low obstacle (no taller than a quarter of the jump‘s distance). You hit the obstacle on a failed check.",
-            "May involve a DC 10 Dexterity (Acrobatics) check to land on your feet in difficult terrain. You land prone on a failed check."
+            "你做以一次難度 DC 10 的力量度「運動」測試 （Strength Athletics)來跳過一個低矮的障礙物，一個不高於跳躍距離的四分之一高度。如果測試失敗，你撞上那個障礙物。",
+            "你做以一次難度 DC 10 的敏捷度「雜技」測試 （Dexterity Acrobatics)來以腳足成功落地於不平面的「困難地形」上。如果測試失敗，你跌倒在地上成伏地狀態。"
         ]
     },
     {
@@ -106,7 +106,7 @@ data_movement = [
         icon: "stone-pile",
         subtitle: "消耗修正: 每 5 呎 加 5 呎",
         reference: "PHB, pg. 182.",
-        description: "Moving in difficult terrain costs an additional 5ft per 5ft of movement",
+        description: "在困難的地形上移動，需要每 5 呎 多消耗 5呎的移動力",
         bullets: [
         ]
     },
@@ -117,8 +117,8 @@ data_movement = [
         description: "拖動或攜帶被抓的生物",
         reference: "PHB, pg. 195.",
         bullets: [
-            "If you move while grappling another creature, your speed is halved, unless the creature is two or more sizes smaller than you.",
-            "See the attack action for how to grapple a creature."
+             "當你抓捕着另一個生物一同移動時，你的速度降低至平時的一半。除非那個生物的身體是你的兩個身碼或更少以下。",
+             "請參考攻擊行動去更明白如何去抓捕一個生物。"
         ]
     }
 ]
